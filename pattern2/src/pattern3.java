6626aece3315614ee641cb4c011c8db0bf21c237
kshitija-mande <146907530+kshitija-mande@users.noreply.github.com>
2024-04-28 19:09:52 +0530

    Add files via upload

---
+++ pattern2/src/pattern3.java
@@ -0,0 +1,13 @@
+public class pattern3 {
+    public static void main(String[] args) {
+        pattern1(4);
+    }
+    public static void pattern1(int n){
+        for(int row=1;row<=n;row++){
+            for(int col=1;col<=row;col++){
+                System.out.print( col +" ");
+            }
+            System.out.println();
+        }
+    }
+}
