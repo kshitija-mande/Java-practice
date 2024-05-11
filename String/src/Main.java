24b1a02fbe32c0689570dae55473558ba8515b77
kshitija-mande <146907530+kshitija-mande@users.noreply.github.com>
2024-05-11 22:00:09 +0530

    Add files via upload

---
+++ String/src/Main.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Revrse {
+    public static void main(String arg[]) {
+       Scanner sc=new Scanner(System.in);
+       String name=sc.next();
+       int len=name.length();
+       String rev ="";
+       for(int i=len-1;i>=0;i--){
+
+       }
+
+        System.out.println(rev);
+
+    }
+}
